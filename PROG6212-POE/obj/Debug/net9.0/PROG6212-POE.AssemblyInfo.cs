4aeadf26428bc7fd755597453afa1f4e855996e4
jarred <st10452547@imconnect.edu.za>
2025-09-17 21:09:52 +0200

    Home Index code

--- PROG6212-POE/obj/Debug/net9.0/PROG6212-POE.AssemblyInfo.cs
+++ PROG6212-POE/obj/Debug/net9.0/PROG6212-POE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROG6212-POE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99fbeecd88a858d357e3808863b07f064ccb41c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROG6212-POE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROG6212-POE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
